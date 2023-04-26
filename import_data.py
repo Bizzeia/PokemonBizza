271e5462e6d1b4fcb018f2d5e48ab17731f3b666
Bizzeia <bommino.leonardo@iisgalvanimi.edu.it>
2023-04-26 10:56:29 +0000

    data importati

--- import_data.py
+++ import_data.py
@@ -12,24 +12,24 @@ mycursor = mydb.cursor()
 
 #Create the table for the csv data (if not exists)
 mycursor.execute("""
-  CREATE TABLE IF NOT EXISTS IF_Paesi (
-    Overall rank FLOAT,
-    Country or region VARCHAR(30) NOT NULL,
+  CREATE TABLE IF NOT EXISTS Felicità.data (
+    Overall_rank FLOAT,
+    Country_or_region VARCHAR(30) NOT NULL,
     Score FLOAT,
-    GDP per capita FLOAT,
-    Social support FLOAT,
-    Healthy life expectancy FLOAT,
-    Freedom to make life choices FLOAT,
+    GDP_per_capita FLOAT,
+    Social_support FLOAT,
+    Healthy_life_expectancy FLOAT,
+    Freedom_to_make_life_choices FLOAT,
     Generosity FLOAT,
-    Perceptions of corruption FLOAT,
+    Perceptions_of_corruption FLOAT
   );""")
 
 #Delete data from the table Clsh_Unit
-mycursor.execute("DELETE FROM CLASH_ROYALE.Clash_Unit")
+mycursor.execute("DELETE FROM Felicità.data")
 mydb.commit()
 
 #Read data from a csv file
-clash_data = pd.read_csv('./cr-unit-attributes.csv', index_col=False, delimiter = ',')
+clash_data = pd.read_csv('./2019.csv', index_col=False, delimiter = ',')
 clash_data = clash_data.fillna('Null')
 print(clash_data.head(20))
 
@@ -37,14 +37,14 @@ print(clash_data.head(20))
 for i,row in clash_data.iterrows():
     cursor = mydb.cursor()
     #here %S means string values 
-    sql = "INSERT INTO CLASH_ROYALE.Clash_Unit VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    sql = "INSERT INTO Felicità.data VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
     cursor.execute(sql, tuple(row))
     print("Record inserted")
     # the connection is not auto committed by default, so we must commit to save our changes
     mydb.commit()
 
 #Check if the table has been filled
-mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit")
+mycursor.execute("SELECT * FROM Felicità.data")
 myresult = mycursor.fetchall()
 
 for x in myresult:
