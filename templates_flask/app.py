6e895ce2661d8d9815fcfc5c3f833c740ddc4194
Bizzeia <bommino.leonardo@iisgalvanimi.edu.it>
2023-04-27 10:52:29 +0000

    ss

--- templates_flask/app.py
+++ templates_flask/app.py
@@ -8,3 +8,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('hello.html', name='Fabio')
+
+
+
+@app.route('/units').
+
+def unitList():
+  Esegui una Query per estrarre tutti i dati sulle unità dal DB. NB! Prima devi connetterti al DB
+  mycursor.execute("SELECT FROM Clash_Unit") myresult = mycursor.fetchall()
+  return render_template('clash_units.html', units=myresult)
